0962e222db53ec804a8d9647ca348e20d221b85e
Cosminflv <cosmin_popovici2012@yahoo.com>
2024-04-06 16:14:55 +0300

    Added logic for capturing

--- Checkers/Checkers/Services/GameBusinessLogic.cs
+++ Checkers/Checkers/Services/GameBusinessLogic.cs
@@ -19,52 +19,6 @@ namespace Checkers.Services
             this.squares = squares;
         }
 
-        private ObservableCollection<Cell> GetPossibleMoves(Cell obj)
-        {
-            ObservableCollection<Cell> possibleMoves = new ObservableCollection<Cell>();
-            if (obj.CellState == ECellState.none)
-            {
-                return possibleMoves;
-            }
-
-            if(obj.CellState == ECellState.red)
-            {
-                if (isInBoard(obj.X - 1, obj.Y - 1))
-                {
-                    if (squares[obj.X - 1][obj.Y - 1].CellState == ECellState.none)
-                    {
-                        possibleMoves.Add(squares[obj.X - 1][obj.Y - 1]);
-                    }
-                }
-                if(isInBoard(obj.X - 1, obj.Y + 1))
-                {
-                    if (squares[obj.X - 1][obj.Y + 1].CellState == ECellState.none)
-                    {
-                        possibleMoves.Add(squares[obj.X - 1][obj.Y + 1]);
-                    }
-                }
-            }
-
-            if (obj.CellState == ECellState.white)
-            {
-                if (isInBoard(obj.X + 1, obj.Y - 1))
-                {
-                    if (squares[obj.X + 1][obj.Y - 1].CellState == ECellState.none)
-                    {
-                        possibleMoves.Add(squares[obj.X + 1][obj.Y - 1]);
-                    }
-                }
-                if (isInBoard(obj.X + 1, obj.Y + 1))
-                {
-                    if (squares[obj.X + 1][obj.Y + 1].CellState == ECellState.none)
-                    {
-                        possibleMoves.Add(squares[obj.X + 1][obj.Y + 1]);
-                    }
-                }
-            }
-            return possibleMoves;
-        }
-
         public void ClickAction(Cell obj)
         {
             if (obj.CellState != ECellState.none)
@@ -83,15 +37,79 @@ namespace Checkers.Services
             }
         }
 
+        // PRIVATE METHODS
         private void Move(Cell cellToMoveOn)
         {
+            Tuple<int, int>? redCapturedPosition = redCaptured(cellToMoveOn);
+            Tuple<int, int>? whiteCapturedPosition = whiteCaptured(cellToMoveOn);
+
+            if(redCapturedPosition != null)
+            {
+                squares[redCapturedPosition.Item1][redCapturedPosition.Item2].CellState = ECellState.none;
+                squares[redCapturedPosition.Item1][redCapturedPosition.Item2].DisplayedImage = squares[redCapturedPosition.Item1][redCapturedPosition.Item2].HiddenImage;
+            }
+
+            if(whiteCapturedPosition != null)
+            {
+                squares[whiteCapturedPosition.Item1][whiteCapturedPosition.Item2].CellState = ECellState.none;
+                squares[whiteCapturedPosition.Item1][whiteCapturedPosition.Item2].DisplayedImage = squares[whiteCapturedPosition.Item1][whiteCapturedPosition.Item2].HiddenImage;
+            }
+
             cellToMoveOn.CellState = selectedSquare!.Item1.CellState;
             cellToMoveOn.DisplayedImage = selectedSquare.Item1.DisplayedImage;
             selectedSquare.Item1.DisplayedImage = selectedSquare.Item1.HiddenImage;
+
+
+            squares[cellToMoveOn.X][cellToMoveOn.Y].CellState = selectedSquare!.Item1.CellState;
+            squares[selectedSquare.Item1.X][selectedSquare.Item1.Y].CellState = ECellState.none;
             selectedSquare.Item1.CellState = ECellState.none;
 
-            Cell cellToUpdate = squares[selectedSquare.Item1.X][selectedSquare.Item1.Y];
-            cellToUpdate.CellState = ECellState.none;
+            selectedSquare = null;
+        }
+
+        private Tuple<int, int>? redCaptured(Cell cellToMoveOn)
+        {
+            if(selectedSquare!.Item1.X - cellToMoveOn.X == 2) // Piece advanced two rows
+            {
+                if (squares[cellToMoveOn.X - 1][cellToMoveOn.Y - 1].CellState == ECellState.white)
+                    return new Tuple<int, int>(cellToMoveOn.X - 1, cellToMoveOn.Y - 1);
+                if (squares[cellToMoveOn.X - 1][cellToMoveOn.Y + 1].CellState == ECellState.white)
+                    return new Tuple<int, int>(cellToMoveOn.X - 1, cellToMoveOn.Y + 1);
+            }
+            return null;
+        }
+
+        private Tuple<int, int>? whiteCaptured(Cell cellToMoveOn)
+        {
+            if (cellToMoveOn.X - selectedSquare!.Item1.X == 2) // Piece advanced two rows
+            {
+                if (squares[cellToMoveOn.X - 1][cellToMoveOn.Y + 1].CellState == ECellState.red)
+                    return new Tuple<int, int>(cellToMoveOn.X - 1, cellToMoveOn.Y + 1);
+                if (squares[cellToMoveOn.X - 1][cellToMoveOn.Y - 1].CellState == ECellState.red)
+                    return new Tuple<int, int>(cellToMoveOn.X - 1, cellToMoveOn.Y - 1);
+            }
+            return null;
+        }
+
+        private ObservableCollection<Cell> GetPossibleMoves(Cell obj)
+        {
+            ObservableCollection<Cell> possibleMoves = new ObservableCollection<Cell>();
+
+            if (obj.CellState == ECellState.none)
+            {
+                return possibleMoves;
+            }
+
+            if (obj.CellState == ECellState.red)
+            {
+                AddPossibleMovesForRed(obj, possibleMoves);
+            }
+            else if (obj.CellState == ECellState.white)
+            {
+                AddPossibleMovesForWhite(obj, possibleMoves);
+            }
+
+            return possibleMoves;
         }
 
         private bool isMovePossible(Cell cellToMoveOn)
@@ -108,6 +126,40 @@ namespace Checkers.Services
         private bool isInBoard(int x, int y)
         {
             return x >= 0 && x <= 7 && y >= 0 && y <= 7;
+        }
+
+        private void AddPossibleMovesForRed(Cell obj, ObservableCollection<Cell> possibleMoves)
+        {
+            AddNormalMove(obj.X - 1, obj.Y - 1, possibleMoves);
+            AddCaptureMove(obj.X - 1, obj.Y - 1, obj.X - 2, obj.Y - 2, ECellState.white, possibleMoves);
+
+            AddNormalMove(obj.X - 1, obj.Y + 1, possibleMoves);
+            AddCaptureMove(obj.X - 1, obj.Y + 1, obj.X - 2, obj.Y + 2, ECellState.white, possibleMoves);
+        }
+
+        private void AddPossibleMovesForWhite(Cell obj, ObservableCollection<Cell> possibleMoves)
+        {
+            AddNormalMove(obj.X + 1, obj.Y - 1, possibleMoves);
+            AddCaptureMove(obj.X + 1, obj.Y - 1, obj.X + 2, obj.Y - 2, ECellState.red, possibleMoves);
+
+            AddNormalMove(obj.X + 1, obj.Y + 1, possibleMoves);
+            AddCaptureMove(obj.X + 1, obj.Y + 1, obj.X + 2, obj.Y + 2, ECellState.red, possibleMoves);
+        }
+
+        private void AddNormalMove(int x, int y, ObservableCollection<Cell> possibleMoves)
+        {
+            if (isInBoard(x, y) && squares[x][y].CellState == ECellState.none)
+            {
+                possibleMoves.Add(squares[x][y]);
+            }
+        }
+
+        private void AddCaptureMove(int x, int y, int captureX, int captureY, ECellState targetState, ObservableCollection<Cell> possibleMoves)
+        {
+            if (isInBoard(x, y) && isInBoard(captureX, captureY) && squares[x][y].CellState == targetState && squares[captureX][captureY].CellState == ECellState.none)
+            {
+                possibleMoves.Add(squares[captureX][captureY]);
+            }
         }
     }
 }
